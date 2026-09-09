16d96c697c89667eb498a69386f07ed4ef5a22c1
Gabriel Erazo <gfranciscoerazom@protonmail.com>
2026-09-08 21:53:24 -0500

    Refactor FilamentUiServiceProvider to use arrow functions for configuration callbacks and improve code readability

--- app/Providers/Filament/FilamentUiServiceProvider.php
+++ app/Providers/Filament/FilamentUiServiceProvider.php
@@ -39,107 +39,103 @@ class FilamentUiServiceProvider extends ServiceProvider
     {
         // When a field has multiple words like "due_date", the label changes from "Due date" to "Due Date".
         Field::configureUsing(function (Field $field) {
-            $field->label(function (Component $component) {
-                return str($component->getName())
+            $field->label(
+                fn (Component $component) => str($component->getName())
                     ->afterLast('.')
                     ->kebab()
                     ->replace(['-', '_'], ' ')
-                    ->ucwords();
-            });
+                    ->ucwords()
+            );
 
-            $field->validationAttribute(function (Component $component) {
-                return $component->getLabel();
-            });
+            $field->validationAttribute(fn (Component $component) => $component->getLabel());
 
             return $field;
         });
 
         // make selects searchable and preloaded by default
-        Select::configureUsing(function (Select $field) {
-            return $field
+        Select::configureUsing(
+            fn (Select $field) => $field
                 ->searchable()
-                ->preload();
-        });
+                ->preload()
+        );
 
         // add sensible min and max so you don't end up with dates like 01/01/0000 or 01/01/3000
-        DatePicker::configureUsing(function (DatePicker $datePicker) {
-            return $datePicker
+        DatePicker::configureUsing(
+            fn (DatePicker $datePicker) => $datePicker
                 ->minDate(Carbon::createFromDate(1500, 1, 1))
-                ->maxDate(now()->addYears(30));
-        });
+                ->maxDate(now()->addYears(30))
+        );
 
         // US based phone input, adjust for different countries
         // SI NO ESTOY MAL HAY UN PLUGIN PARA ESTO
-        // TextInput::macro('phone', function () {
-        //     return $this->mask('(999) 999-9999')
+        // TextInput::macro(
+        //     'phone',
+        //     fn () => $this->mask('(999) 999-9999')
         //         ->prefixIcon('heroicon-o-phone')
         //         ->tel()
         //         ->minLength(14)
         //         ->maxLength(14)
         //         ->validationMessages([
         //             'min' => 'Please enter a valid phone number including area code.',
-        //         ]);
-        // });
+        //         ])
+        // );
 
         // if an action is a modal, do not close by clicking away and default to slideover
-        Action::configureUsing(function (Action $action) {
-            $action
+        Action::configureUsing(
+            fn (Action $action) => $action
                 ->closeModalByClickingAway(false)
-                ->slideOver();
-        });
+                ->slideOver()
+        );
 
         // capitalize the model name in a create action label
-        CreateAction::configureUsing(function (CreateAction $action) {
-            $action
-                ->label(fn (): string => __('filament-actions::create.single.label', ['label' => ucwords($action->getModelLabel())]));
-        });
+        CreateAction::configureUsing(
+            fn (CreateAction $action) => $action
+                ->label(fn (): string => __('filament-actions::create.single.label', ['label' => ucwords($action->getModelLabel())]))
+        );
 
         // various table presets
-        Table::configureUsing(function (Table $table) {
-            return $table
+        Table::configureUsing(
+            fn (Table $table) => $table
                 ->reorderableColumns()
                 ->columnManagerColumns(2)
                 ->columnManagerTriggerAction(fn (Action $action) => $action->button()->label('Columns'))
                 ->filtersTriggerAction(fn (Action $action) => $action->button()->label('Filters')->slideOver()->closeModalByClickingAway(true))
                 ->filtersFormWidth(Width::Small)
-                ->paginationPageOptions([10, 25, 50]);
-        });
+                ->paginationPageOptions([10, 25, 50])
+        );
 
         // allow any column to be toggled
-        Column::configureUsing(function (Column $column) {
-            return $column->toggleable();
-        });
+        Column::configureUsing(fn (Column $column) => $column->toggleable());
 
         // default each text column to be sortable and searchable
-        TextColumn::configureUsing(function (TextColumn $textColumn) {
-            return $textColumn
+        TextColumn::configureUsing(
+            fn (TextColumn $textColumn) => $textColumn
                 ->searchable() // BE CAREFUL, you may end up with 500 errors
-                ->sortable(); // BE CAREFUL, you may end up with 500 errors
-        });
+                ->sortable() // BE CAREFUL, you may end up with 500 errors
+        );
 
         // make notifications last 10 seconds by default
-        Notification::configureUsing(function (Notification $notification) {
-            return $notification->duration(10000);
-        });
+        Notification::configureUsing(fn (Notification $notification) => $notification->duration(10000));
 
         // use your preferred date displays
-        Schema::configureUsing(function (Schema $schema) {
-            return $schema
+        Schema::configureUsing(
+            fn (Schema $schema) => $schema
                 ->defaultDateDisplayFormat('m/d/Y')
                 ->defaultDateTimeDisplayFormat('h:i A')
-                ->defaultTimeDisplayFormat('m/d/Y h:i A');
-        });
+                ->defaultTimeDisplayFormat('m/d/Y h:i A')
+        );
 
-        RichEditor::configureUsing(function (RichEditor $richEditor) {
-            return $richEditor
+        // Configure the RichEditor component
+        RichEditor::configureUsing(
+            fn (RichEditor $richEditor) => $richEditor
                 ->toolbarButtons([
                     ['bold', 'italic', 'underline', 'strike', 'link'],
                     ['h2', 'h3'],
                     ['alignStart', 'alignCenter', 'alignEnd'],
                     ['blockquote', 'codeBlock', 'bulletList', 'orderedList'],
                     ['undo', 'redo'],
-                ]);
-        });
+                ])
+        );
 
         // make wizards persist the current step in the query string and have a submit button
         Wizard::configureUsing(
